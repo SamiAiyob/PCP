13cad30a56e09a194839eb66157b6a0050b7e73a
Sami Aiyob <sami.aiyob@gmail.comgit config --global user.email sami.aiyob@gmail.com>
2024-05-29 15:05:05 +0200

    change in database and adding requirements files

--- backend/settings.py
+++ backend/settings.py
@@ -77,14 +77,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pcpdatabase',
-        'USER': 'pcpapp',
-        'PASSWORD': 'Dci1234!',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
