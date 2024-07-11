520614b807177367dcc36de92848721f8e583b4e
sami <sulsami@protonmail.com>
2024-07-11 22:56:47 +0200

    fixed broken api routes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,12 +3,12 @@ from rest_framework.routers import DefaultRouter
 from .views import *
 
 router = DefaultRouter()
-router.register(r'api/categories', CategoryViewSet)
-router.register(r'api/programmers', ProgrammerViewSet, basename='programmer')
-router.register(r'api/clients', ClientViewSet, basename='client')
+router.register(r'categories', CategoryViewSet)
+router.register(r'programmers', ProgrammerViewSet, basename='programmer')
+router.register(r'clients', ClientViewSet, basename='client')
 
 urlpatterns = [
-    path("api", include(router.urls)),
+    path("api/", include(router.urls)),
     path('api/login/', LoginView.as_view(), name='login'),
     path('api/user/', UserView.as_view(), name='user'),
     path('api/logout/', LogoutView.as_view(), name='logout'),
