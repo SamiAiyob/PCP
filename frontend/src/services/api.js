a05427aa8793e6d5c71b94e78c0729b1aeff162d
sami <sulsami@protonmail.com>
2024-07-19 10:27:58 +0200

    added docker

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,8 +4,4 @@ const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:8000/api",
 });
 
-console.log("loading axiosInstance");
-
-console.log("----->>>>>>>>>>", process.env.REACT_APP_API_BASE_URL);
-
 export default axiosInstance;
